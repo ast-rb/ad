fda86319b968e71d26c924956e4d9ef4eed4c6fd
Astral <ravendv@list.ru>
2013-08-28 20:11:09 +0300

    First commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Advertisement::Application.config.secret_token = '2040c7b7b543dbf664c7ef27993880fb9125bef5720e20344e3e69b3249c553b7e2460fc3e32c7dde6b441a178148b00e62a5c8c4ea0616cd8d6b046bd8cdda3'
